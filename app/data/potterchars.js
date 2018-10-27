7ce9b3b44b50f791431fcb683dae52f55e43b953
Torre Matthew <torre.matthew@gmail.com>
2018-10-27 02:26:14 -0400

    logic for choosing an alter ego built

--- app/data/potterchars.js
+++ app/data/potterchars.js
@@ -1,55 +1,129 @@
 var potterChars = [
-    {
-      charName: "Albus Dumbledoor",
-      shortName: "Dumbledoor",
-      charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/4/40/Albus_Dumbledore_%28HBP_promo%29_3.jpg/revision/latest?cb=20150822232849",
-      scores: [
-          5,
-          5,
-          5,
-          5,
-          5,
-          5,
-          5,
-          5,
-          5,
-          5
-      ]
-    },
-    {
-        charName: "Harry James Potter",
-        shortName: "Harry",
-        charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/a/a9/Dhharryroomhighreso.png/revision/latest?cb=20181009213828",
-        scores: [
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4,
-            4
-        ]
-      },
-    {
-        charName: "Tom (Lord Voldemort) Riddle",
-        shortName: "Voldemort",
-        charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/6/6e/VoldemortHeadshot_DHP1.png/revision/latest?cb=20161203031453",
-        scores: [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
-        ]
-      }
+    [
+        {
+        charName: "Albus Dumbledoor",
+        shortName: "Dumbledoor",
+        charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/4/40/Albus_Dumbledore_%28HBP_promo%29_3.jpg/revision/latest?cb=20150822232849",
+        }
+    ],
+    [
+        {
+            charName: "Harry James Potter",
+            shortName: "Harry",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/a/a9/Dhharryroomhighreso.png/revision/latest?cb=20181009213828",
+        },
+        {
+            charName: "Hermoine Jean Granger",
+            shortName: "Hermoine",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/4/41/Hermionedhface.jpg/revision/latest?cb=20161221044857",
+        }
+    
+    ],
+    [   {
+        charName: "Rubeus Hagrid",
+        shortName: "Hagrid",
+        charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/e/ee/Rubeushagrid.PNG/revision/latest?cb=20161123044204",
+        },
+        {
+            charName: "Luna Lovegood",
+            shortName: "Luna",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/4/49/Luna_profile.jpg/revision/latest?cb=20160902165706",
+        }
+    ],
+    [
+        {
+            charName: "Ron Weasly",
+            shortName: "Ron",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/8/85/Ron_Weasley.jpg/revision/latest?cb=20150828153318",
+        },
+        {
+            charName: "Remus Lupin",
+            shortName: "Lupin",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/e/e2/Remus_Lupin_Deathly_Hallows_promo_image.jpg/revision/latest?cb=20161119235913",
+        },
+        {
+            charName: "Dobby",
+            shortName: "Dobby",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/1/18/DOBBY2.jpg/revision/latest?cb=20161215055153",
+        },
+        {
+            charName: "Neville Longbottom",
+            shortName: "Neville",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/0/04/Harry-potter-and-the-deathly-hallowspart-2.jpg/revision/latest?cb=20161120035218",
+        }
+    ],
+    [
+        {
+            charName: "Sirius Black",
+            shortName: "Sirius",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/7/75/Sirius_Black_profile.jpg/revision/latest?cb=20150918055024",
+        }
+    ],
+    [
+        {
+            charName: "Severus Snape",
+            shortName: "Snape",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/a/a3/Severus_Snape.jpg/revision/latest?cb=20150307193047",
+        }
+    ],
+    [
+        {
+            charName: "Draco Malfoy",
+            shortName: "Draco",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/c/cf/Harry-Potter-The-Deathly-Hallows-Part-II.jpg/revision/latest?cb=20140602031803",
+        },
+        {
+            charName: "Dudley Dursley",
+            shortName: "Dudley",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/6/67/DudleyDH.png/revision/latest?cb=20160830004111",
+        },
+        {
+            charName: "Kreacher",
+            shortName: "Kreacher",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/a/a0/Krech.jpg/revision/latest?cb=20100621114028",
+        }
+    ],
+    [
+        {
+            charName: "Lucius Malfoy",
+            shortName: "Lucius",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/b/b4/Lucius_Malfoy_BoH.png/revision/latest?cb=20120622073504",
+        },
+        {
+            charName: "Barty Crouch Jr",
+            shortName: "Barty",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/b/b4/Lucius_Malfoy_BoH.png/revision/latest?cb=20120622073504",
+        },
+        {
+            charName: "Peter Pettigrew",
+            shortName: "Rat",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/e/e4/Pettigrew_DH1.jpg/revision/latest?cb=20160531030113",
+        }
+    ],
+    [
+        {
+            charName: "Dolores Jane Umbridge",
+            shortName: "Umbridge",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/6/68/Dolores_Umbridge_Deathly_Hallows_promotional_image.jpg/revision/latest?cb=20101127041001",
+        },
+        {
+            charName: "Fenrir Greyback",
+            shortName: "Greybak",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/d/d4/Fenrirgreyback.PNG/revision/latest?cb=20090814213734",
+        },
+        {
+            charName: "Bellatrix Lestrange",
+            shortName: "BellaTrix",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/1/14/BellatrixLestrange.png/revision/latest?cb=20171101034243",
+        }
+    ],
+    [
+        {
+            charName: "Tom (Lord Voldemort) Riddle",
+            shortName: "Voldemort",
+            charphoto: "https://vignette.wikia.nocookie.net/harrypotter/images/6/6e/VoldemortHeadshot_DHP1.png/revision/latest?cb=20161203031453",
+        },
+    ]
   ];
   
   module.exports = potterChars;
--- app/javascript/potterapp.js
+++ app/javascript/potterapp.js
@@ -3,19 +3,26 @@ let user = [];
 
 $(document).ready(function(){
     $('select').formSelect();
-  });
-
+    $('.modal').modal();
   
 
-  
-  let createUserRecord = () => {
-
+  let submitForm = () => {
+    
 
   let seeker = {
     firstname: $("#first_name").val().trim(),
     lastname: $("#last_name").val().trim(),
-    scores: [ $("#q1").find(":selected").val()
-
+    scores: [ 
+    $("#q1").find(":selected").val(),
+    $("#q2").find(":selected").val(),
+    $("#q3").find(":selected").val(),
+    $("#q4").find(":selected").val(),
+    $("#q5").find(":selected").val(),
+    $("#q6").find(":selected").val(),
+    $("#q7").find(":selected").val(),
+    $("#q8").find(":selected").val(),
+    $("#q9").find(":selected").val(),
+    $("#q10").find(":selected").val()
     ] 
     
   }
@@ -23,14 +30,140 @@ $(document).ready(function(){
   user.push(seeker);
   console.log(user);
 
+  let seekerScore = parseInt(user[0].scores[0]) +
+                    parseInt(user[0].scores[1]) +
+                    parseInt(user[0].scores[2]) +
+                    parseInt(user[0].scores[3]) +
+                    parseInt(user[0].scores[4]) +
+                    parseInt(user[0].scores[5]) +
+                    parseInt(user[0].scores[6]) +
+                    parseInt(user[0].scores[7]) +
+                    parseInt(user[0].scores[8]) +
+                    parseInt(user[0].scores[9]);
+
+  console.log(seekerScore);
+  callPotterCharApi(seekerScore);
+
+}
+
+  let callPotterCharApi = (seekerScore) => {
+      
+    $.ajax({
+        url: "/api/potterchars",
+        method: "GET"
+      })
+      .then(function (charData) {
+        console.log(charData);
+       
+        if (seekerScore >= 48 && seekerScore <= 50) {
+          let charGroup = 0;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName); 
+        }
+        else if (seekerScore >= 43 && seekerScore <= 47) {
+          let charGroup = 1;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else if (seekerScore >= 39 && seekerScore <= 42) {
+          let charGroup = 2;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else if (seekerScore >= 34 && seekerScore <= 38) {
+          let charGroup = 3;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else if (seekerScore >= 29 && seekerScore <= 33) {
+          let charGroup = 4;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else if (seekerScore >= 24 && seekerScore <= 28) {
+          let charGroup = 5;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else if (seekerScore >= 19 && seekerScore <= 23) {
+          let charGroup = 6;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else if (seekerScore >= 14 && seekerScore <= 18) {
+          let charGroup = 7;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else if (seekerScore >= 11 && seekerScore <= 13) {
+          let charGroup = 8;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+        else {
+          let charGroup = 9;
+          let groupArraySize = charData[charGroup].length;
+          let char = Math.floor((Math.random() * groupArraySize) + 0);
+          console.log(charData[charGroup][char].charName);
+          console.log(charData[charGroup][char].charphoto);
+          console.log(charData[charGroup][char].shortName);
+        }
+  
+    })
+  
+  }
+  
+  let displayAlterEgoInfo = (group, arraylength) => {
+
   }
 
-  let submitForm = () => {
+  let setUpHandleBars = () => {
+    
+    $.ajax({
+      url: "/api/potterchars",
+      method: "GET"
+    })
+    .then(function (charData) {
+    });
+  }
 
-    $("#find_alter").on("click", function (event) {
+  $(".find_alterego").on("click", function (event) {
       event.preventDefault();
-      createUserRecord();
+      submitForm();
+      
+      
 
   })
 
-}
+
+
+
+
+});
